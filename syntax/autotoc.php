48658526acdddfe6e12b43b758a56a5f46fe3d80
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-11-03 10:08:21 +0900

    implement inlinetoc as a part of auto toc syntax

--- syntax/autotoc.php
+++ syntax/autotoc.php
@@ -28,11 +28,13 @@ class syntax_plugin_headings_autotoc extends DokuWiki_Syntax_Plugin {
         $this->mode = substr(get_class($this), 7);
 
         // syntax pattern
-        $this->pattern[0] = '~~(?:TOC_HERE(?:_CLOSED)?|(?:NO|CLOSE)?TOC)\b.*?~~';
+        $this->pattern[0] = '~~(?:NO|CLOSE)?TOC\b.*?~~';
+        $this->pattern[1] = '{{(?:CLOSED_)?(?:INLINETOC|TOC)\b.*?}}';
     }
 
     function connectTo($mode) {
         $this->Lexer->addSpecialPattern($this->pattern[0], $mode, $this->mode);
+        $this->Lexer->addSpecialPattern($this->pattern[1], $mode, $this->mode);
     }
 
     /**
@@ -45,33 +47,50 @@ class syntax_plugin_headings_autotoc extends DokuWiki_Syntax_Plugin {
         isset($tocTweak) || $tocTweak = $this->loadHelper($this->getPluginName());
 
         // parse syntax
-        preg_match('/^~~([A-Z_]+)/', $match, $m);
-        $start = strlen($m[1]) +2;
-        $param = substr($match, $start+1, -2);
-        list($topLv, $maxLv, $tocClass) = $tocTweak->parse($param);
+        $type = ($match[0] == '~') ? 0 : 1;
+        [$name, $param] = explode(' ', substr($match, 2, -2), 2);
+        [$topLv, $maxLv, $tocClass] = $tocTweak->parse($param);
 
-        switch ($m[1]) {
-            case 'NOTOC':
-                $handler->_addCall('notoc', array(), $pos);
-                $tocDisplay = null; // or 'none'
-                break;
-            case 'CLOSETOC':
-                $tocDisplay = null; // $this->getConf('tocDisplay');
-                $tocState = -1;
-                break;
-            case 'TOC':
-                $tocDisplay = null; // $this->getConf('tocDisplay');
-                break;
-            case 'TOC_HERE':
-                $tocDisplay = -1;
-                break;
-            case 'TOC_HERE_CLOSED':
-                $tocDisplay = -1;
-                $tocState = -1;
-                break;
-        } // end of switch
+        if ($type == 0) { // macro
 
-        return $data = [$ID, $tocDisplay, $tocState, $topLv, $maxLv, $tocClass];
+            switch ($name) {
+                case 'NOTOC':
+                    $handler->_addCall('notoc', array(), $pos);
+                    $tocDisplay = 'none';
+                    $type = 1;
+                    break;
+                case 'CLOSETOC':
+                    $tocDisplay = 'toc'; // 暫定：本来は設定すべきでない
+                    $tocState = -1;
+                    break;
+                case 'TOC':
+                    break;
+            } // end of switch
+
+        } else { // type 1
+            // DokiWiki original TOC or alternative INLINETOC
+            // PLACEHOLDER を出力して、TPL_CONTENT_DISPLAY イベントで置き換える
+
+            $tocState = (substr($name, 0, 6) == 'CLOSED') ? -1 : null;
+            $tocDisplay = isset($tocState) ? substr($name, 6) : $name;  // place holder name
+            $tocDisplay = strtolower($tocDisplay);
+            $tocVariant = ($name == 'inlinetoc') ? 'toc_inline' : null;
+        }
+
+        $props = array_filter( // remove null values
+                [
+                    'display'     => $tocDisplay, // TOC box 表示位置 PLACEHOLDER名
+                    'state'       => $tocState,   // TOC box 開閉状態 -1:close
+                    'toptoclevel' => $topLv,      // TOC 見だし範囲の上位レベル
+                    'maxtoclevel' => $maxLv,      // TOC 見だし範囲の下位レベル
+                    'variant'     => $tocVariant, // TOC box 基本デザイン TOC or INLINETOC
+                    'class'       => $tocClass,   // TOC box 微調整用CSSクラス名
+                ],
+                function($v) {
+                        return !is_null($v);
+                }
+        );
+        return $data = [$type, $props];
     }
 
     /**
@@ -79,55 +98,52 @@ class syntax_plugin_headings_autotoc extends DokuWiki_Syntax_Plugin {
      */
     function render($format, Doku_Renderer $renderer, $data) {
         global $ID;
-        static $call_counter = [];  // counts macro used in the page
-        static $call_ignore  = [];  // flag to be set when decisive macro has resolved
 
-        list($id, $tocDisplay, $tocState, $topLv, $maxLv, $tocClass) = $data;
+        // ページに複数のTOC （見かけは同じ）を許容する
+        // HTML では id="dw__toc" が振られるので、重複識別用に末尾に番号を付加する
+        // 番号付けには 関数 sectionID() を使う。
+        //     dw__toc, dw__inlinetoc1, dw__toc2, dw__inlinetoc3, ...
+        //     <!-- toc_here -->, <!-- inlinetoc1_here -->, <!-- toc2_here -->, ...
+        // 最初のものには番号が付かないことを利用して、初出のみHTMLに置換する。
+        // 
 
-        // skip calls that belong to different page (eg. included pages)
-        //if ($id != $ID) return false;
-
-        // skip unnecessary calls after decisive macro has resolved
-        if (isset($call_ignore[$format][$ID])) {
-            return false;
-        }
-
+        static $tocProps; // toc box properties of the page
+        static $tocCount; // count toc placeholders appeared in the page
+ 
+        [$type, $props] = $data;
+ 
         switch ($format) {
             case 'metadata':
-                // skip macros appeared more than once in a page, except ~~TOC_HERE~~
-                if ($tocDisplay === -1) {
-                    unset($call_counter[$ID]);
+
+                if (!isset($tocProps[$ID])) {
+                    $tocProps[$ID] = [];
                 }
-                if ($call_counter[$ID]++ > 0) {
-                    return false;
+                if ($type == 0) {
+                    $tocProps[$ID] += $props; // if key exists, the value is kept
+                } else {
+                    $tocDisplay = $tocProps[$ID]['display'];
+                    $tocProps[$ID] = array_merge($tocProps[$ID], $props);
+                    if($tocDisplay === 'none') $tocProps[$ID]['display'] = 'none';
                 }
 
                 // store into matadata storage
                 $metadata =& $renderer->meta['plugin'][$this->getPluginName()];
-                $metadata['toc'] = [
-                    'display'     => $tocDisplay,
-                    'state'       => $tocState,
-                    'toptoclevel' => $topLv,
-                    'maxtoclevel' => $maxLv,
-                    'class'       => $tocClass,
-                ];
-
-                if ($tocDisplay === -1) {
-                    $call_ignore[$format][$ID] = true;
-                }
-
+                $metadata['toc'] = $tocProps[$ID];
                 return true;
 
             case 'xhtml':
+                if(!isset($props['display']) || ($props['display'] == 'none')) return false;
+
                 // render PLACEHOLDER, which will be replaced later
                 // through action TPL_CONTENT_DISPLAY event handler
-                if ($tocDisplay === -1) {
-                    $renderer->doc .= '<!-- TOC_HERE -->'.DOKU_LF;
-                    $call_ignore[$format][$ID] = true;
-                    return true;
+                if (!isset($tocCount[$ID])) {
+                    $tocId = $props['display'];
+                    $tocCount[$ID] = 0;
                 } else {
-                    return false;
+                    $tocId = $props['display'].(++$tocCount[$ID]);
                 }
+                $renderer->doc .= '<!-- '.strtoupper($tocId).'_HERE -->'.DOKU_LF;
+                return true;
 
         } // end of switch
         return false;
